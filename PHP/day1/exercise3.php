1783b89f3d3152454285714c65794323c9e90736
Charles <charlesmelki@gmail.com>
2020-06-15 11:34:51 +0200

    Exercises php du 06-15-20

---
+++ PHP/day1/exercise3.php
@@ -0,0 +1,5 @@
+<?php
+/* 06-15-20 3) Créez une page nomfichier.php qui affiche simplement la chaîne de caractères "Hello PHP, nous sommes le " 
+suivie de la date du jour sur le serveur. */
+  echo "Hello PHP, nous sommes le " . date("m/d/y");
+?>
