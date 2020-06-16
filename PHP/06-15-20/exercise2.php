b03361759a001e6217178bd291ee641062c9369b
Charles <charlesmelki@gmail.com>
2020-06-16 11:56:25 +0200

    Re-upload des exercises du 06-15-20 pour renommer le dossier

---
+++ PHP/06-15-20/exercise2.php
@@ -0,0 +1,26 @@
+<?php
+/* 06-15-20 2) Ceci est un code écrit en pure HTML traduisez-le en un script php:
+<!doctype html>
+<html><head>
+<title>bonjour depuis php</title>
+</head>
+<body> bonjour généré dynamiquement en php ! </body>
+</html> */
+?>
+
+<!doctype html>
+<html>
+
+<head>
+  <?php
+  echo "Bonjour depuis php";
+  ?>
+</head>
+
+<body>
+  <?php
+  echo "bonjour généré dynamiquement en php";
+  ?>
+</body>
+
+</html>
