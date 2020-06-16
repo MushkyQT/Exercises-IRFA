1cc89831bd9b05daa09f8acdc1eb6cc674c63c42
Charles <charlesmelki@gmail.com>
2020-06-16 11:56:08 +0200

    Removing because renaming folder structure

--- PHP/day1/exercise3.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* 06-15-20 3) Créez une page nomfichier.php qui affiche simplement la chaîne de caractères "Hello PHP, nous sommes le " 
-suivie de la date du jour sur le serveur. */
-  echo "Hello PHP, nous sommes le " . date("m/d/y");
-?>
