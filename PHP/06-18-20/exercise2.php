c5f3337f97da1d1f8dd8f140c7685f61fe91732a
Charles <charlesmelki@gmail.com>
2020-06-18 19:23:52 +0200

    Changed to ul and li elements.

--- PHP/06-18-20/exercise2.php
+++ PHP/06-18-20/exercise2.php
@@ -2,8 +2,10 @@
 // 2) Ecrivez le code PHP permettant d’afficher, sous forme de liste non ordonnée, les carrés des nombres de 1 à 20 
 $squares = [];
 
+echo "<ul>";
 for ($i = 1; $i <= 20; $i++) {
     $squares[$i] = $i * $i;
-    echo "$i<sup>2</sup> = $squares[$i]<br>";
+    echo "<li>$i<sup>2</sup> = $squares[$i]</li>";
 }
+echo "</ul>";
 ?>
