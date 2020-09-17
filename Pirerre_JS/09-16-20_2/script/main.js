34526e1af045603d2650a162c9c73e2dcfafd69a
Charles <charlesmelki@gmail.com>
2020-09-17 17:26:37 +0200

    Tableaux tableaux tableaux

--- Pirerre_JS/09-16-20_2/script/main.js
+++ Pirerre_JS/09-16-20_2/script/main.js
@@ -17,13 +17,61 @@
 //     console.log(miamSucre[i]);
 // }
 
+// Creer une methode remplirLeTableau
+// cette methode utilise n'importe-quel tableau
+// avec n'importe quel tableau du meme format qu'on lui passe
+
+function remplirLeTableau(tab) {
+    document.getElementById("monBody").innerHTML = ""
+    for (var i = 0; i < tab.length; i++) {
+        document.getElementById("monBody").innerHTML += "<tr><td>" + tab[i][0] + "</td><td>" + tab[i][1] + "</td><td>" + tab[i][2] + "</td></tr>"
+    }
+}
+
 var lesGens = [
     ["Luc", "34 ans", "comptable"],
     ["Marla", "28 ans", "Professeure"],
-    ["Marc", "52 ans", "Chirurgien obstétriste du bulbe rachidien"]
+    ["Marc", "52 ans", "Chirurgien obstétriste du bulbe rachidien"],
+    ["Marcouille", "55 ans", "Bulbe rachidien"],
+    ["Marcette", "544 ans", "Chirurgien du bulbe rachidien"],
+    ["Marco", "39 ans", "Obstétriste du bulbe rachidien"]
 
 ];
 
-for (var i = 0; i < lesGens.length; i++) {
-    document.getElementById("monBody").innerHTML += "<td>" + lesGens[i][0] + "</td><td>" + lesGens[i][1] + "</td><td>" + lesGens[i][2] + "</td>"
+var lesAutresGens = [
+    ["Cedric", "34 ans", "comptable"],
+    ["Paul", "28 ans", "Professeure"],
+    ["Jacques", "52 ans", "Chirurgien obstétriste du bulbe rachidien"],
+    ["Marina", "12 ans", "Capitaine du Titanic"],
+    ["Pascaline", "52 ans", "Chirurgien obstétriste du bulbe rachidien"],
+    ["Maxime", "52 ans", "Dentiste"],
+    ["Lucienne", "52 ans", "Plasticienne"],
+    ["Claudio", "52 ans", "Acteur"]
+
+];
+
+var encoreDesAutresGens = [
+    ["Sonia", "34 ans", "comptable"],
+    ["Michel", "28 ans", "Professeure"],
+    ["Gaetan", "18 ans", "Pilote"],
+    ["Pascal", "12 ans", "Capitaine du Titanic"],
+    ["Sophie", "67 ans", "Chirurgien obstétriste du bulbe rachidien"],
+    ["Patricia", "22 ans", "Couturiere"],
+    ["Mael", "35 ans", "Dirige une secte"],
+    ["Bastien", "52 ans", "Plombier"]
+
+];
+
+document.getElementById("lesGens").onclick = function() {
+    remplirLeTableau(lesGens);
 }
+document.getElementById("lesAutresGens").onclick = function() {
+    remplirLeTableau(lesAutresGens);
+}
+document.getElementById("encoreDesAutresGens").onclick = function() {
+    remplirLeTableau(encoreDesAutresGens);
+}
+
+// for (var i = 0; i < lesGens.length; i++) {
+//     document.getElementById("monBody").innerHTML += "<tr><td>" + lesGens[i][0] + "</td><td>" + lesGens[i][1] + "</td><td>" + lesGens[i][2] + "</td></tr>"
+// }
