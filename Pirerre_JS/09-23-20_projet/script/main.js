28136ac8a3f4f56b70ca5677723f0ed6425107f6
Charles <charlesmelki@gmail.com>
2020-09-24 15:18:01 +0200

    Added functioning content to the columns, done with header area and can move on to body underneath scroll line.

--- Pirerre_JS/09-23-20_projet/script/main.js
+++ Pirerre_JS/09-23-20_projet/script/main.js
@@ -1,4 +1,8 @@
 $(".panneau").click(function () {
     $(this).toggleClass("flexMe");
+    $(this).children("span").toggleClass("hideMe");
+    $(this).children("p").toggleClass("hideMe");
+    $(this).siblings().children("span").addClass("hideMe");
+    $(this).siblings().children("p").addClass("hideMe");
     $(this).siblings().removeClass("flexMe");
 });
