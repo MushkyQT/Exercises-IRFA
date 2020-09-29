80fe2f0a6d0d8a399f191270dbce66e8cf71f87a
Charles <charlesmelki@gmail.com>
2020-09-29 17:42:19 +0200

    Fixed openCart button being visible on the top left corner of the body div before it should exist at all

--- Pirerre_JS/09-28-20/js/main.js
+++ Pirerre_JS/09-28-20/js/main.js
@@ -1,14 +1,21 @@
 $(".plus").click(function () {
-    var myVar = $(".champQuantite").val();
+    var myVar = $(this).prev().val();
     myVar = eval(myVar + "+1");
-    $(".champQuantite").val(myVar);
+    $(this).prev().val(myVar);
 });
 
 $(".moins").click(function () {
-    var myVar = $(".champQuantite").val();
+    var myVar = $(this).next().val();
     myVar = parseInt(myVar);
     if (myVar > 1) {
         myVar = myVar - 1;
     }
-    $(".champQuantite").val(myVar);
+    $(this).next().val(myVar);
 });
+
+var produits = [
+    ["zaba", 2345],
+    ["dreamland", 800],
+];
+
+var panier = [["canard", 2, 5, 10]];
