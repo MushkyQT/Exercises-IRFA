ac0e03cd50d56869aefa5936cd119295135287f8
Charles <charlesmelki@gmail.com>
2020-10-15 12:36:24 +0200

    Removed unneeded stylesheet calls

--- Pierre_PHP/baseDeDonnees/index.php
+++ Pierre_PHP/baseDeDonnees/index.php
@@ -76,7 +76,6 @@ function embellisMaRequete($uneLigneQuOnLuiPasse)
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    <link rel="stylesheet" href="style.css">
     <title>Title</title>
 </head>
 
