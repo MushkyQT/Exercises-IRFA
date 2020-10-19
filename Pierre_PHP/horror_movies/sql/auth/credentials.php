843ec0ea3fec40eb7c3a3b15c45026dae6a627d6
Charles <charlesmelki@gmail.com>
2020-10-19 17:31:13 +0200

    Specify mysql charset as UTF8 to display special characters in Director names

--- Pierre_PHP/horror_movies/sql/auth/credentials.php
+++ Pierre_PHP/horror_movies/sql/auth/credentials.php
@@ -8,3 +8,4 @@ $myConnection = mysqli_connect($host, $user, $pass, $db);
 if (mysqli_connect_error()) {
     die("Connection to database failed.<br>");
 }
+mysqli_set_charset($myConnection, "UTF8");
