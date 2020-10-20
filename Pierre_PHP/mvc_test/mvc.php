a03f34d8416fecdef4bf2d4efe683cc1381c8786
Charles <charlesmelki@gmail.com>
2020-10-20 21:59:47 +0200

    Just tried playing around with OOP style mvc

---
+++ Pierre_PHP/mvc_test/mvc.php
@@ -0,0 +1,34 @@
+<?php
+include('controllers/myController.php');
+include('models/myModel.php');
+include('views/myView.php');
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
+echo $view->output();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Title</title>
+</head>
+
+<body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ Pierre_PHP/mvc_test/views/myView.php
@@ -0,0 +1,17 @@
+<?php
+class View
+{
+    private $model;
+    private $controller;
+
+    public function __construct($controller, $model)
+    {
+        $this->controller = $controller;
+        $this->model = $model;
+    }
+
+    public function output()
+    {
+        return '<p><a href="mvc.php?action=clicked">' . $this->model->string . '</a></p><br><p><a href="mvc.php?action=clicker">' . $this->model->string2 . '</a></p>';
+    }
+}
