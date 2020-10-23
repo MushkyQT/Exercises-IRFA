29630ef31e45bd5b9ba6ef887280a77008519224
Charles <charlesmelki@gmail.com>
2020-10-23 11:04:26 +0200

    Removed required attribute from username and password fields in login form because the signUp button depends on the submission of the entire form. Either split forms or leave username and password non-required. That's fine, right? Maybe

--- Keyboard_vs_Controller/navBarController.php
+++ Keyboard_vs_Controller/navBarController.php
@@ -4,5 +4,5 @@ $navBarStatus = array();
 if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] = true) {
     $navBarStatus["login"] = '<span class="pr-3 text-white">' . $_SESSION['username'] . '</span><button type="submit" name="signOut" class="btn lightOrange signOutBtn mr-3">Sign Out</button>';
 } else {
-    $navBarStatus["login"] = '<div class="form-group"><input type="text" placeholder="Username" class="form-control" name="username" required></div><div class="form-group"><input type="password" placeholder="Password" class="form-control mx-1" name="password" required></div><button type="submit" class="btn lightOrange signInBtn mr-3" name="signIn">Sign In</button><button type="submit" class="btn lightOrange signUpBtn" name="signUp">Sign Up</button>';
+    $navBarStatus["login"] = '<div class="form-group"><input type="text" placeholder="Username" class="form-control" name="username"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control mx-1" name="password"></div><button type="submit" class="btn lightOrange signInBtn mr-3" name="signIn">Sign In</button><button type="submit" class="btn lightOrange signUpBtn" name="signUp">Sign Up</button>';
 }
