fdc84917c3f8c10eee2fdd0667f26fb8fb7a0e90
Charles <charlesmelki@gmail.com>
2020-10-23 14:59:32 +0200

    Moved navbar related checks to onloadViewController

--- Keyboard_vs_Controller/controllers/signInOutAndUpController.php
+++ Keyboard_vs_Controller/controllers/signInOutAndUpController.php
@@ -40,11 +40,3 @@ if (isset($_GET['email'])) {
         return "Invalid verification link, please try again.";
     }
 }
-
-// Create navbar login or logout form
-$navBarStatus = array();
-if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] = true) {
-    $navBarStatus["login"] = '<span class="pr-3 text-white">' . $_SESSION['username'] . '</span><button type="submit" name="signOut" class="btn lightOrange signOutBtn mr-3">Sign Out</button>';
-} else {
-    $navBarStatus["login"] = '<div class="form-group"><input type="text" placeholder="Username" class="form-control" name="username"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control mx-1" name="password"></div><button type="submit" class="btn lightOrange signInBtn mr-3" name="signIn">Sign In</button><button type="submit" class="btn lightOrange signUpBtn" name="signUp">Sign Up</button>';
-}
