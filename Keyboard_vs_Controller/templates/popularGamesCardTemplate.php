178b7324f8470dc45c72f09fedf36f661623cc51
Charles <charlesmelki@gmail.com>
2020-10-24 21:00:47 +0200

    Ignore errors for genres, which some obscure games are missing in their results array

--- Keyboard_vs_Controller/templates/popularGamesCardTemplate.php
+++ Keyboard_vs_Controller/templates/popularGamesCardTemplate.php
@@ -3,7 +3,7 @@
         <img class="card-img-top" src="<?php print $background_image ?>" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title"><?php print $name ?></h5>
-            <p class="card-text"><?php print $genres[0]["name"] ?></p>
+            <p class="card-text"><?php print @$genres[0]["name"] ?></p>
             <form method="post">
                 <button type="submit" class="btn btn-primary" name="gameRequested" value="<?php print $id ?>">Go</button>
             </form>
